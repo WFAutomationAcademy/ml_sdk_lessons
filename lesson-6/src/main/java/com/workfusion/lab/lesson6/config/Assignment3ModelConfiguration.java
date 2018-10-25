67ca5b29542bbf1c0823421f31fe5f19dc9f2013
tdebihina <tdebihina@workfusion.com>
2018-10-25 17:47:58 +0300

    Lesson 6 assignment 3 about NormalizerProcessor

---
+++ lesson-6/src/main/java/com/workfusion/lab/lesson6/config/Assignment3ModelConfiguration.java
@@ -0,0 +1,33 @@
+package com.workfusion.lab.lesson6.config;
+
+import com.workfusion.vds.sdk.api.hypermodel.annotation.ModelConfiguration;
+import com.workfusion.vds.sdk.api.nlp.model.Field;
+
+@ModelConfiguration
+public class Assignment3ModelConfiguration {
+
+    /**
+     * Name of {@link Field} representing an invoice number.
+     */
+    private static final String INVOICE_NUMBER_FIELD = "invoice_number";
+    /**
+     * Name of {@link Field} representing a price.
+     */
+    private static final String PRICE_FIELD = "price";
+    /**
+     * Name of {@link Field} representing a client's name.
+     */
+    private static final String CLIENT_NAME_FIELD = "client_name";
+    /**
+     * Name of {@link Field} representing a client's address city
+     */
+    private static final String CLIENT_ADDRESS_CITY_FIELD = "client_address_city";
+    /**
+     * Name of {@link Field} representing a client's address state
+     */
+    private static final String CLIENT_ADDRESS_STATE_FIELD = "client_address_state";
+
+    // TODO:  PUT YOU CODE HERE
+
+}
+
--- lesson-6/src/test/java/com/workfusion/lab/lesson6/Lesson6Test.java
+++ lesson-6/src/test/java/com/workfusion/lab/lesson6/Lesson6Test.java
@@ -3,13 +3,17 @@
  */
 package com.workfusion.lab.lesson6;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import com.workfusion.lab.lesson6.config.Assignment1ModelConfiguration;
 import com.workfusion.lab.lesson6.config.Assignment2ModelConfiguration;
+import com.workfusion.lab.lesson6.config.Assignment3ModelConfiguration;
 import com.workfusion.lab.utils.BaseLessonTest;
 import com.workfusion.vds.nlp.model.configuration.ConfigurationData;
 import com.workfusion.vds.sdk.api.nlp.annotator.Annotator;
@@ -17,10 +21,12 @@ import com.workfusion.vds.sdk.api.nlp.configuration.FieldInfo;
 import com.workfusion.vds.sdk.api.nlp.configuration.FieldType;
 import com.workfusion.vds.sdk.api.nlp.fe.FeatureExtractor;
 import com.workfusion.vds.sdk.api.nlp.model.EntityBoundary;
+import com.workfusion.vds.sdk.api.nlp.model.Field;
 import com.workfusion.vds.sdk.api.nlp.model.IeDocument;
 import com.workfusion.vds.sdk.api.nlp.model.NamedEntity;
 import com.workfusion.vds.sdk.api.nlp.model.Sentence;
 import com.workfusion.vds.sdk.api.nlp.model.Token;
+import com.workfusion.vds.sdk.api.nlp.processing.Processor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,18 +39,18 @@ public class Lesson6Test extends BaseLessonTest {
      * <p>
      * Annotators:
      * - for both fields add:
-     *   1. a token provider annotator, that injects Token elements into document.
-     *   Use MatcherTokenAnnotator() annotator to return an implementation instance. Regexp: \W+.
-     *   2. - Annotator to create {@link EntityBoundary} elements in document.
-     *   Use EntityBoundaryAnnotator() to return an implementation instance.
-     *
+     * 1. a token provider annotator, that injects Token elements into document.
+     * Use MatcherTokenAnnotator() annotator to return an implementation instance. Regexp: \W+.
+     * 2. - Annotator to create {@link EntityBoundary} elements in document.
+     * Use EntityBoundaryAnnotator() to return an implementation instance.
+     * <p>
      * - for field "invoice_number" add:
      * - a NER provider annotator. Use getJavaPatternRegexNerAnnotator annotator to create NERs in document with
-     *   mentionType 'invoice number' and regex '[0-9]{11}'.
-
+     * mentionType 'invoice number' and regex '[0-9]{11}'.
+     * <p>
      * - for field "client_address" add:
      * - a NER provider annotator. Use AhoCorasickDictionaryNerAnnotator annotator, that injects Token elements into document
-     *   with mentionType 'country' and dictionary "classpath:dictionary/countries.csv".
+     * with mentionType 'country' and dictionary "classpath:dictionary/countries.csv".
      */
     protected List<Annotator> annotators = new ArrayList<>();
 
@@ -55,7 +61,7 @@ public class Lesson6Test extends BaseLessonTest {
 
         // Obtains model configuration for field "invoice_number"
         ConfigurationData configurationData = buildConfiguration(Assignment1ModelConfiguration.class,
-            new FieldInfo.Builder("invoice_number").type(FieldType.FREE_TEXT).build());
+                new FieldInfo.Builder("invoice_number").type(FieldType.FREE_TEXT).build());
 
         // Obtains defined annotators list for field "invoice_number".
         List<Annotator> annotators = getAnnotatorsFromConfiguration(configurationData, 3);
@@ -102,7 +108,7 @@ public class Lesson6Test extends BaseLessonTest {
     /**
      * Assignment 2:
      * <p>
-     * Provide a model configuration for two type of field("FREE_TEXT" and "INVOICE_TYPE"). You need to specify the @Named("featureExtractors") method results to have:
+     * Provide a model configuration for two types of field("FREE_TEXT" and "INVOICE_TYPE"). You need to specify the @Named("featureExtractors") method results to have:
      * <p>
      * FE:
      * - for field type "FREE_TEXT" add IsAllLettersUpperCase()
@@ -135,6 +141,90 @@ public class Lesson6Test extends BaseLessonTest {
         fes = getFEsFromConfiguration(configurationData, 1);
         log("Checking that the IsOnlyNumberInTokenFE() is present for type 'INVOICE_NUMBER'.");
         assertThat(fes.get(0).getClass()).isEqualTo(Assignment2ModelConfiguration.IsOnlyNumberInTokenFE.class);
+    }
+
+    /**
+     * Assignment 3:
+     * <p>
+     * Provide a model configuration for the following field codes: INVOICE_NUMBER_FIELD, PRICE_FIELD, CLIENT_NAME_FIELD,
+     * CLIENT_ADDRESS_CITY_FIELD, CLIENT_ADDRESS_STATE.
+     * You need to specify the @Named("basePostProcessors") method to have following results:
+     * <p>
+     * PP:
+     * - for field type "INVOICE_NUMBER_FIELD" use NormalizerProcessor with TextNormalizer operation to replace all non-digit symbols
+     * - for field type "PRICE_FIELD" use NormalizerProcessor with TextNormalizer operation to remove extra spaces
+     * - for field type "CLIENT_NAME_FIELD" use NormalizerProcessor with TextNormalizer operation to capitalize each word
+     * - for field type "CLIENT_ADDRESS_CITY_FIELD" use NormalizerProcessor with TextNormalizer operation to remove any accent symbols
+     * - for field type "CLIENT_ADDRESS_STATE" use NormalizerProcessor with TextNormalizer operation to convert all letters in capital case
+     */
+    protected List<Processor> processors = new ArrayList<>();
+
+    @Test
+    public void assignment3() throws Exception {
+        // Creates ML-SDK Document to process
+        IeDocument document = getDocument("documents/lesson_6_assignment_3.html");
+
+        // Adds Fields into document based on gold tagging
+        addFields(document, "invoice_number", "price", "client_name", "client_address_city", "client_address_state");
+
+        // Obtains model configuration for field "invoice_number"
+        ConfigurationData invoiceNumberConfiguration = buildConfiguration(Assignment3ModelConfiguration.class,
+                new FieldInfo.Builder("invoice_number")
+                        .type(FieldType.FREE_TEXT)
+                        .build());
+        checkPostProcessorsResults(document, invoiceNumberConfiguration);
+
+        // Obtains model configuration for field "price"
+        ConfigurationData priceConfiguration = buildConfiguration(Assignment3ModelConfiguration.class,
+                new FieldInfo.Builder("price")
+                        .type(FieldType.PRICE)
+                        .multiValue(true)
+                        .build());
+        checkPostProcessorsResults(document, priceConfiguration);
+
+        // Obtains model configuration for field "client_name"
+        ConfigurationData clientNameConfiguration = buildConfiguration(Assignment3ModelConfiguration.class,
+                new FieldInfo.Builder("client_name")
+                        .type(FieldType.PERSON)
+                        .multiValue(false)
+                        .build());
+        checkPostProcessorsResults(document, clientNameConfiguration);
+
+        // Obtains model configuration for field ""client_address_city""
+        ConfigurationData clientAddressCityConfiguration = buildConfiguration(Assignment3ModelConfiguration.class,
+                new FieldInfo.Builder("client_address_city")
+                        .type(FieldType.ADDRESS)
+                        .multiValue(false)
+                        .build());
+        checkPostProcessorsResults(document, clientAddressCityConfiguration);
+
+        // Obtains model configuration for field ""client_address_state""
+        ConfigurationData clientAddressStateConfiguration = buildConfiguration(Assignment3ModelConfiguration.class,
+                new FieldInfo.Builder("client_address_state")
+                        .type(FieldType.ADDRESS)
+                        .multiValue(false)
+                        .build());
+        checkPostProcessorsResults(document, clientAddressStateConfiguration);
+
+    }
+
+    private void checkPostProcessorsResults(IeDocument document, ConfigurationData configurationData) throws IOException {
+        String fieldCode = configurationData.getFieldInfo().getCode();
+        // Process postprocessor
+        List<Processor> processors = getProcessorsFromConfiguration(configurationData); //Assignment post-processor
+
+        // Process annotators for field "<fieldCode>"
+        processPostProcessor(document, processors);
+
+        // Gets all Tokens provided by the annotator to check for field "<fieldCode>"
+        List<Field> tokens = Lists.newArrayList(document.findFields(fieldCode));
+
+        // Checks the provided token with the assignment 1 pattern for field "<fieldCode>"
+        checkElements(tokens, getPostProcessorValidationPath(fieldCode));
+    }
+
+    private String getPostProcessorValidationPath(String fieldCode) {
+        return String.join("", "lesson_6_assignment_3", File.separator, "check_", fieldCode, "_pp.json");
     }
 
 }
